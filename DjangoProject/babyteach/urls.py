b48f6f6feddefb68c4ce277eda6c26504dd74678
wuqiwenpk <604296870@qq.com>
2020-04-29 11:14:24 +0800

    完成三个页面（首页、搜索页、内容页）的基本框架展示

---
+++ DjangoProject/babyteach/urls.py
@@ -0,0 +1,7 @@
+from . import views
+from django.urls import path
+urlpatterns = [
+    path('index', views.index),#首页
+    path('detail', views.detail),#详情页
+    path('search', views.search),#搜索页
+              ]
--- DjangoProject/babyteach/views.py
+++ DjangoProject/babyteach/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
-# Create your views here.
+#首页视图
+def index(request):
+    return render(request, 'babyteach/index.html')
+
+#详情页视图
+def detail(request):
+    return render(request, 'babyteach/detail.html')
+
+#搜索页视图
+def search(request):
+    return render(request, 'babyteach/search.html')
